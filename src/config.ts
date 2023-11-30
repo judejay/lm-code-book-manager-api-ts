460a78c11d77d89fed159aa56adb21572047444b
judejay <judejay26@gmail.com>
2023-11-30 19:48:06 +0000

    feat: use of connection string

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,7 @@
 import * as dotenv from "dotenv";
+
 dotenv.config();
+
 export const CONFIG = {
 	nodeEnv: process.env.NODE_ENV ?? "dev",
 	port: process.env.PORT ?? 3000,
--- src/database/database.ts
+++ src/database/database.ts
@@ -3,19 +3,24 @@ import { CONFIG } from "../config";
 
 // TODO: This should be external config
 export let sequelize : Sequelize;
+const connString = `postgres://${CONFIG.dbUserName}:super-secret-password@127.0.0.1:5432/bookshop`;
+// TODO: Replace the above string with a string built from
+//YOUR process.env.DB_USERNAME etc variables
+
 
 if (process.env.NODE_ENV !== "dev") {
-	sequelize = new Sequelize(
-		CONFIG.dbName ?? "MISSING_DB_NAME_CONFIG",
 
-		CONFIG.dbUserName ?? "MISSING_DB_USERNAME_CONFIG",
+	 sequelize = new Sequelize(connString
+	// 	CONFIG.dbName ?? "MISSING_DB_NAME_CONFIG",
 
-		CONFIG.dbPassword ?? "MISSING_DB_PASSWORD_CONFIG",
-		{
-			host: CONFIG.dbHost ?? "MISSING_DB_HOST_CONFIG",
-			port: parseInt(CONFIG.dbPort as string) ?? "MISSING_DB_PORT_CONFIG",
-			dialect: "postgres",
-		}
+	// 	CONFIG.dbUserName ?? "MISSING_DB_USERNAME_CONFIG",
+
+	// 	CONFIG.dbPassword ?? "MISSING_DB_PASSWORD_CONFIG",
+	// 	{
+	// 		host: CONFIG.dbHost ?? "MISSING_DB_HOST_CONFIG",
+	// 		port: parseInt(CONFIG.dbPort as string) ?? "MISSING_DB_PORT_CONFIG",
+	// 		dialect: "postgres",
+	// 	}
 	);
 } else {
 	sequelize = new Sequelize("sqlite::memory:");
