79e6f563ed825538c25a7c719448f81e9b8c1083
judejay <judejay26@gmail.com>
2023-11-30 17:42:48 +0000

    feat: connecting to Postgres sever

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,4 +1,25 @@
-import { Sequelize } from "sequelize";
+import { Dialect, Sequelize } from "sequelize";
+import { CONFIG } from "../config";
 
 // TODO: This should be external config
-export const sequelize = new Sequelize("sqlite::memory:");
+export let sequelize : Sequelize;
+
+if (process.env.NODE_ENV !== "dev") {
+	sequelize = new Sequelize(
+		CONFIG.dbName ?? "MISSING_DB_NAME_CONFIG",
+
+		CONFIG.dbUserName ?? "MISSING_DB_USERNAME_CONFIG",
+
+		CONFIG.dbPassword ?? "MISSING_DB_PASSWORD_CONFIG",
+		{
+			host: CONFIG.dbHost ?? "MISSING_DB_HOST_CONFIG",
+			port: parseInt(CONFIG.dbPort as string) ?? "MISSING_DB_PORT_CONFIG",
+			dialect: "postgres",
+		}
+	);
+} else {
+	sequelize = new Sequelize("sqlite::memory:");
+
+}
+
+
--- src/services/books.ts
+++ src/services/books.ts
@@ -15,7 +15,6 @@ export const saveBook = async (book: Book) => {
 	return Book.create<Book>(book);
 };
 
-// User Story 4 - Update Book By Id Solution
 export const updateBook = async (bookId: number, book: Book) => {
 	return Book.update(book, {
 		where: {
@@ -25,7 +24,6 @@ export const updateBook = async (bookId: number, book: Book) => {
 };
 
 export const deleteById = async (bookId: number) => {
-//	throw new Error("Function not implemented.");
 const deleted = await Book.destroy({
 	where: {
 		bookId : bookId
